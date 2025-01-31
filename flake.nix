31f8c41f260676f5b380fb031ee53499ee995f76
Alex <alexanderthestudent@yandex.ru>
2025-01-31 17:16:38 +0300

    add lock

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       system = "x86_64-linux";
-      modules = [ ./configuration ];
+      modules = [ ./configuration.nix ];
     };
   };
 }
