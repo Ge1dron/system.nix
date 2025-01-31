11850444546e87249194b675a230ea2cb1f54f83
Alex <alexanderthestudent@yandex.ru>
2025-01-31 17:06:06 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Ge1dron's dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      system = "x86_64-linux";
+      modules = [ ./configuration ];
+    };
+  };
+}
